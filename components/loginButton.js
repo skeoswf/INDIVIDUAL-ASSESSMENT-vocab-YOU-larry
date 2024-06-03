7d00c488af64a4426bc1036244c6e3c635138371
Skeo <skeo051600@gmail.com>
2024-06-03 17:02:18 -0500

    Add login functionality to the app.

--- components/loginButton.js
+++ components/loginButton.js
@@ -1,9 +1,14 @@
 import { signIn } from '../utils/auth';
 
 // GOOGLE LOGIN BUTTON
+// prettier-ignore
 const loginButton = () => {
-  const domString = '<button id="google-auth" class="btn btn-danger">GOOGLE LOGIN</button>';
-  document.querySelector('#login-form-container').innerHTML = domString;
+  const domString = `
+  <h1 id="loginHeader">vocabYOUlary get it LMFAOOO</h1>
+  <br>
+  <button id="google-auth" class="btn btn-danger">login please    login i n eed a good career</button>
+  `;
+  document.querySelector('#app').innerHTML = domString;
   document.querySelector('#google-auth').addEventListener('click', signIn);
 };
 
