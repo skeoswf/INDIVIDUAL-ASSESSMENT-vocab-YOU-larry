e17ed316e4f3e6883e5a9faf020d4ad8f3d674fa
Skeo <skeo051600@gmail.com>
2024-06-04 19:31:07 -0500

    Create navbar Complete login/logout functionality

--- utils/startapp.js
+++ utils/startapp.js
@@ -1,9 +1,13 @@
 import navigationEvents from '../events/navigationEvents';
 import domBuilder from '../components/domBuilder';
+import navBar from '../components/shared/navBar';
+import logoutButton from '../components/logoutButton';
 
 const startApp = () => {
   domBuilder();
+  navBar();
   navigationEvents();
+  logoutButton();
 };
 
 export default startApp;
