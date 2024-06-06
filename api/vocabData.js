4a8bfa1ca9ed21ec892d7d40bff01caae6dde5d8
Skeo <skeo051600@gmail.com>
2024-06-06 00:29:56 -0500

    Add vocabData.js
    Add initial GET promise to get vocab data from API

---
+++ api/vocabData.js
@@ -0,0 +1,22 @@
+import client from '../utils/client';
+
+const endpoint = client.databaseURL;
+
+// prettier-ignore
+const getVocab = () => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/vocab.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        resolve(Object.values(data));
+      } else {
+        resolve([]);
+      }
+    })
+    .catch(reject);
+});
