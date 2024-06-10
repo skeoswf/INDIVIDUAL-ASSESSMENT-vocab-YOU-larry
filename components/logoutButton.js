edba35a1cbd45c9a3e3aa0b1b198fff0281d9073
Skeo <skeo051600@gmail.com>
2024-06-09 20:01:42 -0500

    Add navbar styling

--- components/logoutButton.js
+++ components/logoutButton.js
@@ -2,9 +2,9 @@ import { signOut } from '../utils/auth';
 
 // prettier-ignore
 const logoutButton = () => {
-  const domString = '<button id="google-auth" class="btn btn-danger">sign out</button>';
+  const domString = '<button id="google-auth-test" class="btn btn-danger">sign out</button>';
   document.querySelector('#logout-button').innerHTML = domString;
-  document.querySelector('#google-auth').addEventListener('click', signOut);
+  document.querySelector('#google-auth-test').addEventListener('click', signOut);
 };
 
 export default logoutButton;
